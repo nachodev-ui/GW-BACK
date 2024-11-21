b10447c66e5e9e49886c539302a98acaed821ff1
nachodev-ui <ig.cisternas@duocuc.cl>
2024-11-20 21:53:13 -0300

    feat(api): router -> transbankRouter. POST - GET - PUT methods.

---
+++ routes/transbankRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { transbankPayment, confirmTransaction, getTransaction } from '../controllers/transbankController.js';
+
+const transbankRouter  = express.Router();
+
+transbankRouter .post('/create', transbankPayment);
+transbankRouter .get('/get/:token', getTransaction);
+transbankRouter .put('/confirm/:token', confirmTransaction);
+
+export default transbankRouter;
