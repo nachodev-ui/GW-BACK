fbdfc41f5bc123c8e148ab07207263368870f85e
nachodev-ui <ig.cisternas@duocuc.cl>
2024-11-20 21:49:54 -0300

    feat(api): Main Khipu payment methods (createOrder, getOrder...) created

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,4 @@
-import { createPayment } from '../services/khipuService.js';
+import { createPayment, getPayment, deletePayment, refundPayment } from '../services/khipuService.js';
 
 export const createOrder = async (req, res, next) => {
   try {
@@ -14,3 +14,47 @@ export const createOrder = async (req, res, next) => {
     next(error); // Manejo de errores globales
   }
 };
+
+export const getOrder = async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+
+    const paymentResponse = await getPayment(paymentId);
+
+    res.status(200).json({
+      message: 'Pago obtenido exitosamente',
+      payment: paymentResponse,
+    });
+  } catch (error) {
+    next(error); // Manejo de errores globales
+  }
+}
+
+export const cancelOrder = async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+
+    await deletePayment(paymentId);
+
+    res.status(204).json({
+      message: 'Pago cancelado exitosamente',
+    });
+  } catch (error) {
+    next(error); // Manejo de errores globales
+  }
+}
+
+export const refundOrder = async (req, res, next) => {
+  try {
+    const { paymentId } = req.params;
+
+    await refundPayment(paymentId);
+
+    res.status(204).json({
+      message: 'Pago reembolsado exitosamente',
+    });
+  } catch (error) {
+    next(error); // Manejo de errores globales
+  }
+}
+
