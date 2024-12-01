3b997a5414ba8c18b5df6a287aed33a3f56d144c
nachodev-ui <ig.cisternas@duocuc.cl>
2024-12-01 19:14:20 -0300

    feat: /finished POST with handleReturnUrl controller

--- routes/transbankRoutes.js
+++ routes/transbankRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { transbankPayment, confirmTransaction, getTransaction } from '../controllers/transbankController.js';
+import { transbankPayment, confirmTransaction, getTransaction, handleReturnUrl } from '../controllers/transbankController.js';
 
 const transbankRouter  = express.Router();
 
 transbankRouter.post('/create', transbankPayment);
 transbankRouter.get('/get/:token', getTransaction);
 transbankRouter.put('/confirm/:token', confirmTransaction);
+
+// Ruta para el returnUrl
+transbankRouter.post('/finished', handleReturnUrl);
 
 export default transbankRouter;
