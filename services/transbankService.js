7837c5900bbee57966ab7f51bb29fa94112a5360
nachodev-ui <ig.cisternas@duocuc.cl>
2024-12-03 22:28:38 -0300

    refactor: rollback to returnUrl

--- services/transbankService.js
+++ services/transbankService.js
@@ -7,7 +7,7 @@ export const tbkTransaction = async ({ buyOrder, sessionId, amount, returnUrl })
       buy_order: buyOrder,
       session_id: sessionId,
       amount,
-      return_url: `${returnUrl}?token_ws={{token}}`,
+      return_url: returnUrl,
     };
 
     // Realiza la solicitud POST a la API de Transbank
